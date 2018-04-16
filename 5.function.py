0e6e59738f3b351d8bbccc910169f85c03cc027d
Yi Liu Nat <1009066669@qq.com>
2018-04-16 10:08:38 +0800

    function

---
+++ 5.function.py
@@ -0,0 +1,19 @@
+#coding=utf-8
+# 函数代码块以def关键词开头，后接函数名称和圆括号
+# 传入参数和自变量放括号中间
+# 函数第一行可加入文档字符串用于函数说明
+# 函数内容以冒号开始
+# return 结束函数，选择性的返回一个值给调用方
+
+# 语法
+# def function_name (parameters):
+#   "函数说明"
+#   function_suite
+#   return [expression]
+
+def printme(str):
+    "打印传入的字符串"
+    print str + "嘻嘻嘻"
+    return
+
+printme("我要调用函数！")
